a843c8c95d9e460ed2e5fab5589b14ae77fcad6b
suchot <403620968@qq.com>
2019-09-16 21:13:42 +0800

    linux

--- docs/offer/x.py
+++ docs/offer/x.py
@@ -1,3 +1,6 @@
-a = ['n','a']
-b = ['z',17]
-print dict(zip(a,b))
+def p(x):
+    y = reduce(lambda x,y: x*y, map(int, str(x)))
+    return y and x % y
+def q(x):
+    return p(x) and p(x+1)
+print sum(q(x) for x in xrange(2019))
